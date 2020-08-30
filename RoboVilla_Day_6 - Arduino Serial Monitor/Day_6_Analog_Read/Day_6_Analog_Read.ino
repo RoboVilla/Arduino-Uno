de5cd9f3775982ecb94c84a2347c2f61cc067753
RoboVilla <69468836+RoboVilla@users.noreply.github.com>
2020-08-30 09:56:38 +0530

    Update Day_6_Analog_Read.ino

--- RoboVilla_Day_6 - Arduino Serial Monitor/Day_6_Analog_Read/Day_6_Analog_Read.ino
+++ RoboVilla_Day_6 - Arduino Serial Monitor/Day_6_Analog_Read/Day_6_Analog_Read.ino
@@ -1,3 +1,6 @@
+//RoboVilla YouTube Channel
+//Watch Full Video URL - https://www.youtube.com/watch?v=v-hVqOMDJqw&list=PLAk2XwfozpbItNJEPJK9Kgac-SF31-Kp_&index=6
+
 int button = A0;
 int led = 11;
 void setup() {
