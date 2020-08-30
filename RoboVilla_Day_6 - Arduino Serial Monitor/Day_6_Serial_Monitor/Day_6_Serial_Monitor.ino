2b6b7d0a28385810380f625e46dfda5cc88dd744
RoboVilla <69468836+RoboVilla@users.noreply.github.com>
2020-08-30 09:57:23 +0530

    Update Day_6_Serial_Monitor.ino

--- RoboVilla_Day_6 - Arduino Serial Monitor/Day_6_Serial_Monitor/Day_6_Serial_Monitor.ino
+++ RoboVilla_Day_6 - Arduino Serial Monitor/Day_6_Serial_Monitor/Day_6_Serial_Monitor.ino
@@ -1,3 +1,6 @@
+//RoboVilla YouTube Channel
+//Watch Full Video URL - https://www.youtube.com/watch?v=v-hVqOMDJqw&list=PLAk2XwfozpbItNJEPJK9Kgac-SF31-Kp_&index=6
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Welcome to RoboVilla");
